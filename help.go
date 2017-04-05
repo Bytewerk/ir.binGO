a371693d3a0fbe2afb5e2cc870448ee265ade3c2
linux <linux@localhost.localdomain>
2017-04-05 15:51:26 +0200

    Added set to help text

--- help.go
+++ help.go
@@ -2,8 +2,10 @@ package main
 
 const HELPTEXT =
 `The following commands are available:
+    - set [key] [value]                  Sets specific session variabls. Use "list variables"
+                                         to see a list of available variables
     - execute [command|preset] [name]    Executes a command or a preset
-    - list [commands|presets]            Lists available commands or presets
+    - list [commands|presets|variables]  Lists available commands or presets
     - inspect [command|preset] [name]    Inspects a command or a preset showing either
                                          the code sent out by the command or the commands
                                          a preset executes
